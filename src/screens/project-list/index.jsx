c9d5cdbce00e14574623bff3543e81b09ec0e223
spellbind <2591754854@qq.com>
2022-12-03 11:54:24 +0800

    feat: add Custom（useMount、useDebounce）

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,42 +1,44 @@
 import React from "react"
 import { List } from "./list"
 import { SearchPanel } from "./search-panel"
-import {  useEffect, useState } from 'react'
-import { clearnObject } from "../../utils"
+import { useEffect, useState } from 'react'
+import { clearnObject, useDebounce } from "../../utils"
 import * as qs from "qs"
+import { useMount } from './../../utils/index';
 
 const apiUrl = "http://localhost:3004"
 
-export const ProjectListScreen = ()=>{
-  const [param,setParm] = useState({
-    name:'',
-    personId:''
+export const ProjectListScreen = () => {
+  const [param, setParm] = useState({
+    name: '',
+    personId: ''
   })
-  
-  const [users,setUsers] = useState([])
+  const debouncedParam = useDebounce(param,2000)
 
-  const [list,setList] = useState([])
+  const [users, setUsers] = useState([])
+
+  const [list, setList] = useState([])
   // 获取项目列表接口
-  useEffect(() =>{
+  useEffect(() => {
 
-    fetch(`${apiUrl}/projects?${qs.stringify(clearnObject(param))}`).then(async response =>{
-      if(response.ok){//如果请求成功,保存项目列表数据
+    fetch(`${apiUrl}/projects?${qs.stringify(clearnObject(debouncedParam))}`).then(async response => {
+      if (response.ok) {//如果请求成功,保存项目列表数据
         setList(await response.json())
       }
-    }) 
-  },[param])//当param改变时获取
+    })
+  }, [debouncedParam])//当debouncedParam改变时获取
 
-  //初始化user
-  useEffect(()=>{
-    fetch(`${apiUrl}/users`).then(async response =>{
-      if(response.ok){//如果请求成功,保存项目列表数据
+  //用自定义hook 来 初始化user
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async response => {
+      if (response.ok) {//如果请求成功,保存项目列表数据
         setUsers(await response.json())
       }
-    }) 
-  },[])//只初始化一次
+    })
+  })//只初始化一次
 
   return <div>
-    <SearchPanel users={users} param ={param} setParm={setParm}/>
-    <List users={users} list={list}/>
+    <SearchPanel users={users} param={param} setParm={setParm} />
+    <List users={users} list={list} />
   </div>
 }
--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,4 +1,6 @@
-/*清除空对象*/
+import { useEffect, useState } from 'react';
+
+//1. 清除空对象
 
 // 考虑 value 等于0的情况，!!value 意思是将value转化成布尔值
 export const isNull = (value) =>value === 0 ? false : !value
@@ -14,4 +16,30 @@ export const clearnObject = (object) =>{
     }
   })
   return result;
+}
+
+// 2.初始化users
+export const useMount = (callback) =>{
+  useEffect(() => {
+    callback()
+  },[])
+}
+
+//3.通过自定义hook实现Debounce
+export const useDebounce = (value,delay) =>{
+  // 为value 设置一个新的状态，改变其更新频率
+  const [deboucedValue,setDebouncedValue] = useState(value)
+
+  useEffect(()=>{
+    //每次在value 变化以后，设置一个定时器
+    const timeout = setTimeout(()=>setDebouncedValue(value),delay);
+
+    //每次在上一个userEffect处理完以后再在运行,
+    //意味着每次都清理上一个userEffect设置的定时器，只有最后设置的定时器没被清理，会被保留下来
+    return ()=>clearTimeout(timeout);
+
+  },[value,delay])//每当 value 变化时执行 
+
+  // 将 value 成功转化成了 deboucedValue ,也降低了其更新频率
+  return deboucedValue;
 }
