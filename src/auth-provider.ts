8864473448053f5b73addffedcbb1171a9fe62bc
spellbind <2591754854@qq.com>
2022-12-08 22:06:51 +0800

    add auth-provider.ts

---
+++ src/auth-provider.ts
@@ -0,0 +1,44 @@
+import React from "react";
+import { User } from "./screens/project-list/search-panel";
+
+// 在真实开发环境中，如果使用firebase这种第三方auth服务的话，本文件不需要开发者开发。
+const localStorageKey = "__auth_provider_token__";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const getToken = () => window.localStorage.getItem(localStorageKey);
+
+export const handleUserResponse = ({ user }: { user: User }) => {
+  window.localStorage.setItem(localStorageKey, user.token || "");
+  return user;
+};
+
+export const login = (data: { username: string; passsword: string }) => {
+  fetch(`${apiUrl}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (response) => {
+    if (response.ok) {
+      return handleUserResponse(await response.json());
+    }
+  });
+};
+
+export const register = (data: { username: string; passsword: string }) => {
+  fetch(`${apiUrl}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (response) => {
+    if (response.ok) {
+      return handleUserResponse(await response.json());
+    }
+  });
+};
+
+export const logout = () => window.localStorage.removeItem(localStorageKey);
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,36 +1,45 @@
-import React from "react"
+import React from "react";
 
 // 暴露 User 接口实现复用
-export interface User{
-  id:string;
-  name:string;
-  email:string;
-  title:string;
-  organization:string;
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  title: string;
+  organization: string;
+  token: string;
 }
 
-interface SearchPanelProp{
-  users:User[],
-  param:{
-    name:string;
-    personId:string;
-  },
-  setParam:(param:SearchPanelProp['param']) => void;
+interface SearchPanelProp {
+  users: User[];
+  param: {
+    name: string;
+    personId: string;
+  };
+  setParam: (param: SearchPanelProp["param"]) => void;
 }
 
-export const SearchPanel = ({users,param,setParam}:SearchPanelProp)=>{
-
-  return <form>
-    <div>
-      <input type="text" value={param.name} 
-        onChange ={e =>setParam({...param,name:e.target.value})} />
-      <select value={param.personId} 
-        onChange ={e =>setParam({...param,personId:e.target.value})}>
+export const SearchPanel = ({ users, param, setParam }: SearchPanelProp) => {
+  return (
+    <form>
+      <div>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(e) => setParam({ ...param, name: e.target.value })}
+        />
+        <select
+          value={param.personId}
+          onChange={(e) => setParam({ ...param, personId: e.target.value })}
+        >
           <option value={""}>负责人</option>
-          {
-            users.map(user => <option key={user.id} value={user.id}>{user.name}</option>)
-          }
-      </select>
-    </div>
-  </form>
-}
+          {users.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </form>
+  );
+};
