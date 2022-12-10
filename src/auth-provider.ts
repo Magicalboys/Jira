3e693b8182d4ab3b1973bbcb05fca6c51ee2060b
spellbind <2591754854@qq.com>
2022-12-11 01:52:48 +0800

    Update

--- src/auth-provider.ts
+++ src/auth-provider.ts
@@ -8,11 +8,13 @@ const apiUrl = process.env.REACT_APP_API_URL;
 
 export const getToken = () => window.localStorage.getItem(localStorageKey);
 
+// 将获取到的user对象中自动生成的 token 映射到到 localStorage 里面
 export const handleUserResponse = ({ user }: { user: User }) => {
   window.localStorage.setItem(localStorageKey, user.token || "");
   return user;
 };
 
+// 将username和password都通过 fetch 传递给服务端 并且将user对象中自动生成的 token 存到 localStorage 里面
 export const login = (data: { username: string; passsword: string }) => {
   fetch(`${apiUrl}/login`, {
     method: "POST",
